bc23bb4a6ed3f8a42d84bf6e2f929c389ef86ee9
daniel <9087597+daankrs@users.noreply.github.com>
2018-08-22 14:40:10 +0200

    Changes in Perlin noise

--- perlin1/sketch.js
+++ perlin1/sketch.js
@@ -1,5 +1,6 @@
 var startingPoint = 0
-var stepSize = 0.00 4
+var startingPoint2 = 100000
+var stepSize = 0.001
 var speed = 0.005
 
 function setup(){
@@ -7,14 +8,13 @@ function setup(){
 }
 
 function draw(){
-  background(51)
-  stroke(255)
+  background(255)
+  stroke(0)
   strokeWeight(1)
   noFill()
   beginShape()
 
   var xOffset = startingPoint;
-
   for(var x = 0; x < width; x++){
     var y = noise(xOffset) *height
     vertex(x,y)
@@ -24,13 +24,38 @@ function draw(){
       fill('#3498db')
       noStroke()
       ellipse(width/2,noise(xOffset) *height,20,20)
-      stroke(255)
+      stroke(0)
       noFill()
     }
 
     xOffset += stepSize
   }
   endShape()
+
+
+  beginShape()
+
+  var xOffset2 = startingPoint2;
+
+  for(var x = 0; x < width; x++){
+    var y = noise(xOffset) *height
+    vertex(x,y)
+
+    // Draw a coloured circle in the middle of the screen
+    if(x === width/2){
+      fill('#e74c3c')
+      noStroke()
+      ellipse(width/2,noise(xOffset) *height,20,20)
+      stroke(0)
+      noFill()
+    }
+
+    xOffset += stepSize
+  }
+  endShape()
+
+
+
 
 
 
